4c03e3dd8d1273a381408ba57956ea35316abc60
saifulislam106 <saifulislam106915@gmail.com>
2025-01-02 13:12:25 +0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+
+import './App.css'
+import { Button } from './components/ui/button';
+import { decrement, increment } from './redux/feature/counter/counterSlice';
+import { useAppDispatch, useAppSelector } from './redux/hook';
+
+function App() {
+  const dispatch = useAppDispatch();
+  const {count}= useAppSelector((state) => state.counter);
+
+  const handleIncrement = (amount:number) => {
+    dispatch(increment(amount));
+  };
+  const handleDecrement = (amount:number) => {
+    dispatch(decrement(amount));
+  };
+
+  return (
+    <>
+      <h1 className='font-bold text-4xl'>Redux counter</h1>
+      <div className="flex gap-10 items-center justify-center mt-12">
+        <Button onClick={()=>handleDecrement(1)}>decrement</Button>
+        <p>{count}</p>
+        <Button onClick={()=>handleIncrement(2)}>increment</Button>
+      </div>
+    </>
+  );
+}
+
+export default App
