41c7c2b6f53cd961e8b3292e5855ac3f3fdb31d2
saifulislam106 <saifulislam106915@gmail.com>
2025-01-02 15:40:39 +0600

    navber commited

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,31 @@
 
-import './App.css'
-import { Button } from './components/ui/button';
-import { decrement, increment } from './redux/feature/counter/counterSlice';
-import { useAppDispatch, useAppSelector } from './redux/hook';
+
+// import { Button } from './components/ui/button';
+import Navbar from './components/ui/layout/Navbar';
+// import { decrement, increment } from './redux/feature/counter/counterSlice';
+// import { useAppDispatch, useAppSelector } from './redux/hook';
 
 function App() {
-  const dispatch = useAppDispatch();
-  const {count}= useAppSelector((state) => state.counter);
+  // const dispatch = useAppDispatch();
+  // const {count}= useAppSelector((state) => state.counter);
 
-  const handleIncrement = (amount:number) => {
-    dispatch(increment(amount));
-  };
-  const handleDecrement = (amount:number) => {
-    dispatch(decrement(amount));
-  };
+  // const handleIncrement = (amount:number) => {
+  //   dispatch(increment(amount));
+  // };
+  // const handleDecrement = (amount:number) => {
+  //   dispatch(decrement(amount));
+  // };
 
   return (
     <>
-      <h1 className='font-bold text-4xl'>Redux counter</h1>
+      {/* <h1 className='font-bold text-4xl'>Redux counter</h1>
       <div className="flex gap-10 items-center justify-center mt-12">
         <Button onClick={()=>handleDecrement(1)}>decrement</Button>
         <p>{count}</p>
         <Button onClick={()=>handleIncrement(2)}>increment</Button>
+      </div> */}
+      <div className='container mx-auto'>
+        <Navbar/>
       </div>
     </>
   );
---
+++ src/components/ui/layout/Navbar.tsx
@@ -0,0 +1,11 @@
+
+
+function Navbar() {
+    return (
+        <div>
+            <h3 className="font-bold text-3xl">Redux<span className="text-gray-500">Shadcn</span></h3>
+        </div>
+    );
+}
+
+export default Navbar;
